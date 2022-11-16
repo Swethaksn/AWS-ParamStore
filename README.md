e0c9aacfe59b3803fc39648a82e5cbe27a3d0868
Swetha K S N <swetha.ksn@logmein.com>
2022-11-16 17:54:54 +0530

    AWS ParamStore

---
+++ README.md
@@ -0,0 +1,14 @@
+#### Environment Variables
+
+AWS_ACCESS_KEY_ID=<>
+
+AWS_SECRET_ACCESS_KEY=<>
+
+AWS_SESSION_TOKEN=<>
+
+AWS_EC2_METADATA_DISABLED=true
+
+AWS_REGION=<>
+
+
+
---
+++ src/main/java/com/aws/paramstore/controller/ParamsController.java
@@ -0,0 +1,21 @@
+package com.aws.paramstore.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParamsController {
+
+	@Value("${testparam}")
+	private String testParam;
+
+	@GetMapping(value = "/params")
+	public ResponseEntity<?> getParams(HttpServletRequest request) {
+		return ResponseEntity.status(HttpStatus.OK).body("Testparam: " + testParam);
+	}
+}
